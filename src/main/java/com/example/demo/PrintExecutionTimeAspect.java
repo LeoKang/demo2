abf5fa5b4649f08ad731c5d445b66976f613a4c7
seungjin <xeungjin@nate.com>
2026-06-23 13:02:49 +0900

    32 page - @Before

--- src/main/java/com/example/demo/PrintExecutionTimeAspect.java
+++ src/main/java/com/example/demo/PrintExecutionTimeAspect.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class PrintExecutionTimeAspect {
+
+    @Before("@annotation(PrintExecutionTime)")
+    public void beforePrintExecutionTime(JoinPoint joinPoint) {
+        System.out.println("do something before " + joinPoint.toShortString() + " with " + joinPoint.getArgs().length + " args. ");
+    }
+
     @Around("@annotation(PrintExecutionTime)")
     public Object printExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long start = System.currentTimeMillis();
